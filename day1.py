4d08565ae7dcf9e7f89121774af99207b657d6c6
bahaa <blainefire00@gmail.com>
2023-02-15 19:29:38 +0300

    Done Day 1

---
+++ day1.py
@@ -0,0 +1,50 @@
+#EXCERTISE 1 Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function
+#Write your code below this line 👇 
+
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
+
+#EXCERTISE 2  Look at the code in the code editor on the right. There are errors in all of the lines of code. Fix the code so that it runs without errors.
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+#EXCERTISE 3
+#Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+#Write your code below this line 👇
+a = input('What is your name? ')
+print(len(a))
+
+#EXCERTISE 4  Write a program that switches the values stored in the variables a and b.
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+temp = a
+a = b
+b = temp 
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
+
+
+
+
+
