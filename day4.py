bd0c9b61d8d0aed979aeb1a955b93647e1f91263
Bahaa Hani <blainefire00@gmail.com>
2023-03-26 00:33:43 +0300

    day 4 complete

--- day4.py
+++ day4.py
@@ -3,7 +3,7 @@ import random
 num = random.randint(1, 3)
 print(num)
 
-# Path: day4.py
+# day4
 ## head tail game
 headTail = input("Heads or Tails? ")
 rand= random.randint(0, 1)
@@ -24,3 +24,79 @@ sates_of_america = ['Deleware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connec
 
 states = random.choice(sates_of_america)
 print(states)
+#who is going to pay the bill game
+
+str = input('who is going to pay the bill?').split(',')
+choice = random.choice(str)
+print(f'{choice} you are going to pay the bill')
+
+#treasure map game
+print('welcome to treasure island')
+
+print('your mission is to find the treasure')
+
+
+#rock paper scissors game
+
+rock_paper_scissors = input("Rock, Paper, Scissors? ").lower()
+rand = random.randint(0, 2)
+if rand == 0:
+    print("Rock")
+elif rand == 1:
+    print("Paper")
+else:
+    print("Scissors")
+
+if rock_paper_scissors == "rock" and rand == 0:
+    print("Draw")
+elif rock_paper_scissors == "rock" and rand == 1:
+    print("You lose")
+elif rock_paper_scissors == "rock" and rand == 2:
+    print("You win")
+elif rock_paper_scissors == "paper" and rand == 0:
+    print("You win")
+elif rock_paper_scissors == "paper" and rand == 1:
+    print("Draw")
+elif rock_paper_scissors == "paper" and rand == 2:
+    print("You lose")
+elif rock_paper_scissors == "scissors" and rand == 0:
+    print("You lose")
+elif rock_paper_scissors == "scissors" and rand == 1:
+    print("You win")
+elif rock_paper_scissors == "scissors" and rand == 2:
+    print("Draw")
+
+#password generator game
+password = ""
+for i in range(0, 3):
+    password += random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+")
+
+print(password)
+
+import random
+
+# Password Generator Project
+password = ""
+for i in range(0, 3):
+    password += random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+")
+print(password)
+
+# EASY: Letters + Numbers
+# MEDIUM: Letters + Numbers + Symbols
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+password = ""
+for i in range(0, nr_letters):
+    password += random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+for i in range(0, nr_symbols):
+    password += random.choice("!@#$%^&*()_+")
+for i in range(0, nr_numbers):
+    password += random.choice("1234567890")
+print(password)
+
+
+# HARD: Letters + Numbers + Symbols
+
+ 
