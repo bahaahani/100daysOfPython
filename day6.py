00512f6f2a951593cdc4ca4d474fb32cff79531b
bahaa <blainefire00@gmail.com>
2023-03-26 19:55:53 +0300

    day 6 and 7

---
+++ day6.py
@@ -0,0 +1,12 @@
+#day 6
+# for loop
+
+# for i in range(1, 10):
+
+
+#     print(i)
+
+
+
+# for i in range(1, 10, 2):
+
---
+++ day7.py
@@ -0,0 +1,14 @@
+# day 7
+# for loop
+
+# for i in range(1, 10):
+move()  
+
+def move():
+    for i in range(1, 10):
+        print(i)
+        
+#     print(i)
+
+
+# code on plexit
