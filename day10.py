796ff9f34410788c4068c773ba84ca29326aafd4
bahaa <blainefire00@gmail.com>
2023-04-14 02:54:22 +0300

    day11 start

--- day10.py
+++ day10.py
@@ -1,3 +1,20 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
 # function with output
 def format_name():
     first_name = input("What is your first name? ").title()
@@ -11,7 +28,8 @@ format_name()
 def format_name(first_name, last_name):
     full_name = first_name + " " + last_name
     return full_name.title()
-
+    """_summary_
+    """
 def is_leap(year):
     if year % 4 == 0:
         if year % 100 == 0:
@@ -23,3 +41,30 @@ def is_leap(year):
             return True
     else:
         return False
+    
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for symbol in operations:
+    print(symbol)
+operations_symbol = input("Pick an operation from the line above: ")
+calculation_function = operations[operations_symbol]
+print(f"{num1} {operations_symbol} {num2} = {calculation_function(num1, num2)}")
