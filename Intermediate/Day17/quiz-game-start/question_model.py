c3e956b3f64833bc106f6ecf18700c286cad9539
bahaa <blainefire00@gmail.com>
2023-04-19 20:15:16 +0300

    part 4 complete

--- Intermediate/Day17/quiz-game-start/question_model.py
+++ Intermediate/Day17/quiz-game-start/question_model.py
@@ -1,6 +1,5 @@
 class Question:
-    
-    
-    def __init__(self,text,ans):
+
+    def __init__(self, text, ans):
         self.text = text
         self.answer = ans
--- Intermediate/Day17/quiz-game-start/quiz_brain.py
+++ Intermediate/Day17/quiz-game-start/quiz_brain.py
@@ -1,9 +1,19 @@
-from question_model import Question
-from data import question_data
+class QuizBrain:
 
-# question_bank = []
-# for question in question_bank:
-#     question_text = question["text"]
-#     question_answer = question["answer"]
-#     new_question = Question(question_text, question_answer)
-#     question_bank.append(new_question)
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number +=1
+        user_ans = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        correct_ans = self.current_question.answer
+        self.check_ans(user_ans, correct_ans)
+
+    def still_has_questions(self):
+        return len(self.question_list) > self.question_number
+
+    def check_ans(self, user_ans,correct_ans):
+        return user_ans == correct_ans
+
