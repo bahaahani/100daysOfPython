2cd6f5f9286fa42577f46a08e58db9fb73001d70
bahaa <blainefire00@gmail.com>
2023-04-20 16:43:22 +0300

    complete day21

--- Intermediate/Day20 &Day21/food.py
+++ Intermediate/Day20 &Day21/food.py
@@ -1,6 +1,9 @@
 from turtle import Turtle
 import random
+
+
 class Food(Turtle):
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
@@ -14,4 +17,3 @@ class Food(Turtle):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
-        
