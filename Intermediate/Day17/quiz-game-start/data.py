de7aee2515d32ceb3d1f0d487c686404390d6e5c
bahaa <blainefire00@gmail.com>
2023-04-20 03:01:29 +0300

    day 17 complete

--- Intermediate/Day17/quiz-game-start/data.py
+++ Intermediate/Day17/quiz-game-start/data.py
@@ -12,3 +12,106 @@ question_data = [
 {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
 {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
 ]
+
+question_data2=[
+    {
+"category": "General Knowledge",
+"type": "boolean",
+"difficulty": "easy",
+"question": "The Great Wall of China is visible from the moon.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "General Knowledge",
+"type": "boolean",
+"difficulty": "easy",
+"question": "It is automatically considered entrapment in the United States if the police sell you illegal substances without revealing themselves.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "General Knowledge",
+"type": "boolean",
+"difficulty": "easy",
+"question": "Nutella is produced by the German company Ferrero.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "General Knowledge",
+"type": "boolean",
+"difficulty": "easy",
+"question": "You can legally drink alcohol while driving in Mississippi.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"category": "General Knowledge",
+"type": "boolean",
+"difficulty": "easy",
+"question": "Pluto is a planet.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "General Knowledge",
+"type": "boolean",
+"difficulty": "easy",
+"question": "The Lego Group was founded in 1932.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"category": "General Knowledge",
+"type": "boolean",
+"difficulty": "easy",
+"question": "Romanian belongs to the Romance language family, shared with French, Spanish, Portuguese and Italian. ",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"category": "General Knowledge",
+"type": "boolean",
+"difficulty": "easy",
+"question": "&quot;27 Club&quot; is a term used to refer to a list of famous actors, musicians, and artists who died at the age of 27.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"category": "General Knowledge",
+"type": "boolean",
+"difficulty": "easy",
+"question": "Scotland voted to become an independent country during the referendum from September 2014.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "General Knowledge",
+"type": "boolean",
+"difficulty": "easy",
+"question": "Ping-Pong originated in England",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+}
+]
--- Intermediate/Day17/quiz-game-start/quiz_brain.py
+++ Intermediate/Day17/quiz-game-start/quiz_brain.py
@@ -3,17 +3,24 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.score = 0
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        self.question_number +=1
-        user_ans = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
-        correct_ans = self.current_question.answer
+        user_ans = input(f"Q.{self.question_number}: {current_question.text} (True/False): ").lower()
+        correct_ans = self.question_list[self.question_number].answer
         self.check_ans(user_ans, correct_ans)
+        self.question_number +=1
 
     def still_has_questions(self):
-        return len(self.question_list) > self.question_number
+        return   self.question_number < len(self.question_list)
 
     def check_ans(self, user_ans,correct_ans):
-        return user_ans == correct_ans
+        if user_ans == correct_ans:
+            print("\nYou got that right\n")
+            self.score += 1
+        else:
+            print(f"\nthats wrong! {correct_ans} is correct answer\n")
+        print(f"Current Score is {self.score}/{self.question_number}")
+        
 
