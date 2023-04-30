604d32ae8aca5ca27ad01ce8a210f2915a8f49e1
Bahaa Hani <blainefire00@gmail.com>
2023-05-01 01:16:02 +0300

    day22 mid

---
+++ Intermediate/Day22/main.py
@@ -0,0 +1,30 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_on = True
+while game_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+screen.exitonclick()
