6e1b756059dd59911575a8e89841f8d666bb9457
Bahaa Hani <blainefire00@gmail.com>
2023-05-01 17:03:40 +0300

    complete Day23

--- Intermediate/Day23/main.py
+++ Intermediate/Day23/main.py
@@ -7,8 +7,30 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 game_is_on = True
+screen.listen()
+screen.onkey(player.moveUp, "Up")
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            scoreboard.gameOver()
+            game_is_on = False
+
+    if player.is_at_finish_line():
+        player.goStart()
+        car_manager.level_up()
+        scoreboard.increase_level()
+        
+
+
+screen.exitonclick()
--- Intermediate/Day23/player.py
+++ Intermediate/Day23/player.py
@@ -1,7 +1,26 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.penup()
+        self.goStart()
+        self.setheading(90)
+        self.finish_line = FINISH_LINE_Y
+
+    def moveUp(self):
+        self.forward(MOVE_DISTANCE)
+
+    def goStart(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() >= FINISH_LINE_Y:
+            return True
