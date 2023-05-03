7e0a70c4ad313fd02b41d90c7af65ed091ea7ef5
Bahaa Hani <blainefire00@gmail.com>
2023-05-03 12:47:08 +0300

    Day26 Complete

--- Intermediate/Day26/main.py
+++ Intermediate/Day26/main.py
@@ -32,3 +32,57 @@ passed_students = {
 }
 print(passed_students)
 
+
+numbers = [1, 1, 2, 3, 4, 4, 5, 5, 5]
+# squared_numbers = [new_items for items in list]
+squared_numbers = [num*num for num in numbers]
+
+print(squared_numbers)
+even_numbers = [num for num in numbers if num % 2 == 0]
+print(even_numbers)
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = { day:(temp_c * 9/5 +32) for (day,temp_c) in weather_c.items()}
+print(weather_f)
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict={row.letter:row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+[print(item) for item in output_list]
