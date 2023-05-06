4159dd04c122cec24f4132a95bc3b8e55d8778c4
bahaa <blainefire00@gmail.com>
2023-05-06 13:44:51 +0300

    Day27 Start

---
+++ Intermediate/Day27/main.py
@@ -0,0 +1,27 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI Program")
+
+window.title("Hello this is dr ali alsaffar")
+window.minsize(500,100)
+my_label=tkinter.Label(text="My Label", font=("Arial",24,"italic"))
+my_label.pack()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+window.mainloop()
