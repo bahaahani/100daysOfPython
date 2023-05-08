2c7ef2ae5d06ab2bf2da634452b90bb6688d85d7
bahaa <blainefire00@gmail.com>
2023-05-08 21:56:58 +0300

    27

--- Intermediate/Day27/main.py
+++ Intermediate/Day27/main.py
@@ -1,27 +1,103 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 
 window.title("Hello this is dr ali alsaffar")
-window.minsize(500,100)
-my_label=tkinter.Label(text="My Label", font=("Arial",24,"italic"))
+window.minsize(500, 100)
+
+my_label = Label(text="My Label", font=("Arial", 24, "italic"))
 my_label.pack()
 
+# button
+def button_clicked():
+    print("uWu i got clicked")
+    my_label["text"] = "UWU"
+    my_input = input.get()
+    my_label.config(text=my_input)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+button = Button(text="Click mee uWu", command=button_clicked)
+button.pack()
+# entry 
+input = Entry(width=10)
+input.pack()
+print(input.get())
+window.mainloop()
+
+# *args: Positional Variable-Length Arguments
+def add(*args):
+    # print(args[1])
+
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
+# print(add(3, 5, 6, 2, 1, 7, 4, 3))
+
+# **kwargs: Keyworded Variable-Length Arguments
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    # print(n)
+
+calculate(2, add=3, multiply=5)
+
+# How to use a **kwargs dictionary safely
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
+
+my_car = Car(make="Nissan", model="Skyline")
+print(my_car.model)
+
+
+from tkinter import *
+
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
+
+#Label
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
+
+#Button
+button = Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+#Entry
+input = Entry(width=10)
+print(input.get())
+input.grid(column=3, row=2)
+
+
+
+
+
+
+
+
+
 window.mainloop()
