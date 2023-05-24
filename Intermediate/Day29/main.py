9adfb4c2d8c58ae28d80169380028a101915ed53
“Bahaa <blainefire00@gmail.com>
2023-05-24 23:48:57 +0300

    h

--- Intermediate/Day29/main.py
+++ Intermediate/Day29/main.py
@@ -43,6 +43,7 @@ window.title("Password Manager")
 window.config(padx=50, pady=50)
 canvas = Canvas(width=200, height=200)
 # logo =PhotoImage(file="logo.png")hmm
+
 canvas.create_image(100, 100)#image=logo) hmmm
 canvas.grid(column=1, row=0)
 
