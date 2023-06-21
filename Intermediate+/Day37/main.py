da455d61eff2d0d0c5324c7ab04c45636fb333b7
bahaa <blainefire00@gmail.com>
2023-06-22 00:14:23 +0300

    day37 done

--- Intermediate+/Day37/main.py
+++ Intermediate+/Day37/main.py
@@ -1,9 +1,9 @@
 import requests
-
+from datetime import datetime
 pixela_endpoint = "https://pixe.la/v1/users"
-
+TOKEN = "MEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOW"
 user_params = {
-    "token":"MEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOWMEOW",
+    "token":TOKEN,
     "username":"meowmeowmeow",
     "agreeTermsOfService":"yes",
     "notMinor":"yes"
@@ -20,5 +20,28 @@ graph_config = {
     "unit":"hours",
     "type":"float",
     "color":"ajisai",
-    "timezone":"Bahrain/Manama"
 }
+
+headers = {
+    "X-USER-TOKEN":TOKEN,
+}
+
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/meowmeowmeow/graphs/graph1"
+datetime_now = datetime.now()
+date = datetime_now.strftime("%Y%m%d")
+pixel_data = {
+    "date": date,
+    "quantity":"1",
+}
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
+
+# response = requests.put(url=f"{pixel_creation_endpoint}/{date}", json={"quantity":"2"}, headers=headers)
+# print(response.text)
+
+response = requests.delete(url=f"{pixel_creation_endpoint}/{date}", headers=headers)
+print(response.text)
