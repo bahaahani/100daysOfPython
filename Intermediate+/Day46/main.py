a8e093b250743243423a1c4ff63b9a18fcba18fb
Bahaa Hani <57040462+bahaahani@users.noreply.github.com>
2023-07-18 19:12:52 +0300

    spotify music maker

--- Intermediate+/Day46/main.py
+++ Intermediate+/Day46/main.py
@@ -1,49 +1,44 @@
-# import requests
-# from bs4 import BeautifulSoup
-# requests = requests.get("https://news.ycombinator.com/")
-# #print(requests.text)
-
-
-# soup = BeautifulSoup(requests.text, "html.parser")
-# articles = soup.find_all(name="span", class_="titleline")
-
-# # article_tag= soup.find(name="span", class_="titleline")
-# # print(article_tag.getText())
-
-# # article_link = article_tag.find(name="a")
-# # print(article_link.get("href"))
-# # article_upvote = soup.find(name="span", class_="score")
-# # print(article_upvote.getText())
-# articles_text = []
-# articles_link = []
-
-# for article_tag in articles:
-#     text = article_tag.getText()
-#     articles_text.append(text)
-#     link = article_tag.find(name="a")
-#     articles_link.append(link.get("href"))
-    
-    
-
-# # articles_text = [article.getText() for article in articles]
-# # articles_link = [articles.get("href") for articles in articles]
-
-# articles_upvote = [int(score.getText().split()[0]) for score in  soup.find_all(name="span", class_="score")]
-# largest_number = max(articles_upvote)
-# print(articles_text)
-# print(articles_link)
-# print(articles_upvote)
-
-# print(f"{articles_text[articles_upvote.index(largest_number)]}\n{articles_link[articles_upvote.index(largest_number)]}")
-
-url = "https://www.empireonline.com/movies/features/best-movies-2/"
-import requests
 from bs4 import BeautifulSoup
-response = requests.get(url=url)
-website_html = response.text
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-soup = BeautifulSoup(website_html, "html.parser")
- 
-allmovies= soup.find_all(name="h3", class_="jsx-1913936986")
+# Scraping Billboard 100
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
 
-print(allmovies)
+#Spotify Authentication
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id="767dca89acd04e3dbf34ec9f38c30b30",
+        client_secret="9bfc0593dda5480282dac449ffd83d4f",
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
+
+#Searching Spotify for songs by title
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+#Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+#Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
